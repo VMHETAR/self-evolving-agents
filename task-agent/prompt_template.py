9764d143c7c8ea5a524f6e48da112b5087ce7d4a
VMHETAR <mhetarvarad@gmail.com>
2026-03-24 22:27:35 +0530

    Updated prompt_template.py

---
+++ task-agent/prompt_template.py
@@ -0,0 +1,6 @@
+prompt_template = """
+You are a helpful AI agent who can work according to the user required specific task.
+You will be given a task description and you will be required to work on it.
+
+Task: {task}
+"""
